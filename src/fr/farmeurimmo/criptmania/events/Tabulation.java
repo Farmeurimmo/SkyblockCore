aa7bd9b704a285aed3cdae1aa7b3b86a6afa0d5b
Robin M <farmeurimmo@gmail.com>
2021-08-04 11:42:20 +0200

    Ajout de suggestion de commandes

--- src/fr/farmeurimmo/criptmania/events/Tabulation.java
+++ src/fr/farmeurimmo/criptmania/events/Tabulation.java
@@ -13,7 +13,10 @@ public class Tabulation implements Listener {
 	public void OnTabulation(PlayerCommandSendEvent e) {
 		e.getCommands().clear();
 		e.getCommands().addAll(Arrays.asList("is","shop","sellall","warp","warps","menu","farm2win","spawn","hat","feed","fly","dailyfly"
-				,"craft","enchantement","build"));
+				,"craft","enchantement","wiki","money","baltop"));
+		if(e.getPlayer().hasPermission("*")) {
+			e.getCommands().addAll(Arrays.asList("gm","gmc","gms","gmsp","gma","ban","mute","ban-ip","pardon","pardon-ip"));
+		}
 	}
 
 }
