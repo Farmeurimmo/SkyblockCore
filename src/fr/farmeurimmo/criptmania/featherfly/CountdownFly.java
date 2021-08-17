996c8c27fb8bb7240a2793019f93aa053c69cf34
Robin M <farmeurimmo@gmail.com>
2021-08-17 14:46:09 +0200

    Fix d'un bug sur la perte de fly temporaire

--- src/fr/farmeurimmo/criptmania/featherfly/CountdownFly.java
+++ src/fr/farmeurimmo/criptmania/featherfly/CountdownFly.java
@@ -44,18 +44,27 @@ public static HashMap<UUID, Integer> fly = new HashMap<>();
     }
     public static void CountDown(Player player) {
     	if(player.isOnline() && fly.containsKey(player.getUniqueId())) {
+    		int TimeLeft = getCooldown(player.getUniqueId());
+    		
     	if(getCooldown(player.getUniqueId()) - 1 != -1 && getCooldown(player.getUniqueId()) >= 0) {
-    	int TimeLeft = getCooldown(player.getUniqueId()) - 1;
-    	if(player.getAllowFlight() == false) {
+    		
+    		if(!player.getWorld().getName().equalsIgnoreCase("world")) {
+    	TimeLeft = TimeLeft - 1;
+    		}
+    	
+    	if(player.getAllowFlight() == false && !player.getWorld().getName().equalsIgnoreCase("world")) {
     	player.setAllowFlight(true);
     	}
+    	
     	int timeforconv = TimeLeft;
     	int nHours = (timeforconv%86400)/3600;
         int nMin = ((timeforconv%86400)%3600) /60;
         int nSec =(((timeforconv%86400)%3600)%60);
+        
     	String messagetimeleft = "§aTemps restant: " + nHours + ":" + nMin + ":" + nSec;
     	CountdownFly.setCooldown(player.getUniqueId(), TimeLeft);
     	SendActionBar.SendActionBarMsg(player, messagetimeleft);
+    	
     	Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Bukkit.getPluginManager().getPlugin("SkyblockCore"), new Runnable() {
 			public void run() {
 				CountDown(player);
