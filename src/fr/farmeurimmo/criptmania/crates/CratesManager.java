614757af99f7203b53a8537857ac22cbd2143ed6
Robin M <farmeurimmo@gmail.com>
2021-08-22 15:49:25 +0200

    Début preview boxe légendaire

--- src/fr/farmeurimmo/criptmania/crates/CratesManager.java
+++ src/fr/farmeurimmo/criptmania/crates/CratesManager.java
@@ -80,24 +80,24 @@ public class CratesManager implements Listener {
 	public String LegCratesLoot(Player player) {
 		String loot = null;
 		Random rand = new Random();
-            int n = rand.nextInt(16);
+            int n = rand.nextInt(28);
             if (n == 0){
             	loot = "x1 Pioche légendaire T4";
             	Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "itemleg give " + player.getName() + " 4");
             }
-            if (n == 1 || n == 2){
+            if (n == 1){
             	loot = "x1 Pioche légendaire T3";
             	Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "itemleg give " + player.getName() + " 3");
             }
-            if (n == 3 || n == 4 || n == 5){
+            if (n == 2 || n == 3){
             	loot = "x1 Pioche légendaire T2";
             	Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "itemleg give " + player.getName() + " 2");
             }
-            if (n == 6 || n == 7 || n == 8 || n == 9){
+            if (n == 4 || n == 5 || n == 6){
             	loot = "x1 Pioche légendaire T1";
             	Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "itemleg give " + player.getName() + " 1");
             }
-            if (n == 10 || n == 11){
+            if (n == 7 || n == 8){
             	if(!player.hasPermission("dieu")) {
             	loot = "Grade §9Dieu";
             	Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " +player.getName() + " parent set Dieu server=skyblock");
@@ -105,7 +105,7 @@ public class CratesManager implements Listener {
             		loot = "reroll";
             	}
             }
-            if (n == 12){
+            if (n == 9){
             	if(!player.hasPermission("zeus")) {
             	loot = "Grade §bZeus";
             	Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " +player.getName() + " parent set Zeus server=skyblock");
@@ -113,17 +113,45 @@ public class CratesManager implements Listener {
             		loot = "reroll";
             	}
             }
-            if (n == 13 || n == 14 || n == 15){
+            if (n == 10 || n == 11 || n == 12){
             	loot = "x1 Spawneur à Iron Golem";
             	Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "silkspawners add "+player.getName()+" iron_golem");
             }
-            if (n == 16 || n == 17){
+            if (n == 13 || n == 14){
             	loot = "x2 Spawneurs à Iron Golem";
             	Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "silkspawners add "+player.getName()+" iron_golem 2");
             }
-            if (n == 18){
+            if (n == 15){
             	loot = "x3 Spawneurs à Iron Golem";
             	Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "silkspawners add "+player.getName()+" iron_golem 3");
+            }
+            if (n == 16 || n == 17){
+            	loot = "x1 SellChest";
+            	Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "chests give "+player.getName()+" sell_chest");
+            }
+            if (n == 18){
+            	loot = "x2 SellChest";
+            	Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "chests give "+player.getName()+" sell_chest 2");
+            }
+            if (n == 19 || n == 20){
+            	loot = "x1 Balise";
+            	player.getInventory().addItem(new ItemStack(Material.BEACON, 1));
+            }
+            if (n == 21){
+            	loot = "x2 Balises";
+            	player.getInventory().addItem(new ItemStack(Material.BEACON, 2));
+            }
+            if (n == 22 || n == 23|| n == 24){
+            	loot = "x8 Blocs de netherite";
+            	player.getInventory().addItem(new ItemStack(Material.NETHERITE_BLOCK, 8));
+            }
+            if (n == 25 || n == 26){
+            	loot = "x12 Blocs de netherite";
+            	player.getInventory().addItem(new ItemStack(Material.NETHERITE_BLOCK, 12));
+            }
+            if (n == 27){
+            	loot = "x16 Blocs de netherite";
+            	player.getInventory().addItem(new ItemStack(Material.NETHERITE_BLOCK, 16));
             }
 		
             return loot;
@@ -149,11 +177,29 @@ public class CratesManager implements Listener {
 	public static void LegCratesPreview(Player player) {
         Inventory inv = Bukkit.createInventory(null, 36, "§6Boxe légendaire");
         
-    	ItemStack custom1 = new ItemStack(Material.GRASS_BLOCK, 1);
+    	ItemStack custom1 = new ItemStack(Material.NETHERITE_PICKAXE, 1);
 		ItemMeta meta1 = custom1.getItemMeta();
-		meta1.setDisplayName("§6EN dev");
+		meta1.setDisplayName("§6§6Pioche légendaire §8| §eTier §c4");
 		custom1.setItemMeta(meta1);
 		inv.setItem(10, custom1);
+		
+		ItemStack custom2 = new ItemStack(Material.NETHERITE_PICKAXE, 1);
+		ItemMeta meta2 = custom2.getItemMeta();
+		meta2.setDisplayName("§6§6Pioche légendaire §8| §eTier §c3");
+		custom2.setItemMeta(meta2);
+		inv.setItem(11, custom2);
+		
+		ItemStack custom3 = new ItemStack(Material.NETHERITE_PICKAXE, 1);
+		ItemMeta meta3 = custom3.getItemMeta();
+		meta3.setDisplayName("§6§6Pioche légendaire §8| §eTier §c2");
+		custom3.setItemMeta(meta3);
+		inv.setItem(12, custom3);
+		
+		ItemStack custom4 = new ItemStack(Material.NETHERITE_PICKAXE, 1);
+		ItemMeta meta4 = custom4.getItemMeta();
+		meta4.setDisplayName("§6§6Pioche légendaire §8| §eTier §c1");
+		custom4.setItemMeta(meta4);
+		inv.setItem(13, custom4);
 		
 		
 		
