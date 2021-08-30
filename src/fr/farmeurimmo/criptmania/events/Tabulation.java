df49c370a2a66cc897a6af325df3820075e8226c
Robin M <farmeurimmo@gmail.com>
2021-08-30 15:28:20 +0200

    Ajout de commande aux joueurs lors de tabulation

--- src/fr/farmeurimmo/criptmania/events/Tabulation.java
+++ src/fr/farmeurimmo/criptmania/events/Tabulation.java
@@ -11,6 +11,7 @@ public class Tabulation implements Listener {
 	
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void OnTabulation(PlayerCommandSendEvent e) {
+		if(!e.getPlayer().hasPermission("*")) {
 		e.getCommands().clear();
 		e.getCommands().addAll(Arrays.asList("is","shop","sellall","warp","warps","menu","farm2win","spawn","hat","feed","fly","dailyfly"
 				,"craft","enchantement","wiki","money","baltop","bar","tpa","tpacancel","tpyes","tpno","pay","challenges","c","atout",
@@ -18,6 +19,7 @@ public class Tabulation implements Listener {
 		if(e.getPlayer().hasPermission("*")) {
 			e.getCommands().addAll(Arrays.asList("gm","gmc","gms","gmsp","gma","ban","tempban","mute","ban-ip","pardon","pardon-ip","build","lp","lpb","afkmine","clear","checkfly",
 					"silkspawners","sanctionset","invsee","redstone","datafile","key","sanctionset","chests","itemleg"));
+		}
 		}
 	}
 }
