19a21aab5577dca9c35ff8102cc136ef097887be
Robin M <farmeurimmo@gmail.com>
2022-02-06 13:44:29 +0100

    Compteur TAB avec les metadatavalues

--- src/main/java/fr/verymc/scoreboard/TABManager.java
+++ src/main/java/fr/verymc/scoreboard/TABManager.java
@@ -1,8 +1,9 @@
 package main.java.fr.verymc.scoreboard;
 
-import fr.verymc.Commands.CommandMod;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
 
 public class TABManager {
 
@@ -10,10 +11,18 @@ public class TABManager {
       SendTab();
    }
 
+   public static ArrayList<String> Vanished = new ArrayList<String>();
+
    public void SendTab(){
       Bukkit.getServer().getScheduler().scheduleAsyncDelayedTask(Bukkit.getPluginManager().getPlugin("SkyblockCore"), new Runnable() {
          public void run() {
-            int online = Bukkit.getOnlinePlayers().size()-CommandMod.Vanish.size();
+            Vanished.clear();
+            for(Player p : Bukkit.getOnlinePlayers()){
+               if(!p.getMetadata("vanished").isEmpty()){
+                  Vanished.add(p.getName());
+               }
+            }
+            int online = Bukkit.getOnlinePlayers().size()-Vanished.size();
             for(Player player : Bukkit.getOnlinePlayers()){
                player.setPlayerListHeaderFooter("\n§f• §6§lVery§f§lMc §f•\n\n§7Connecté(s) §7▸ §a"+online+
                  "\n§7Une question ? contacte un §9§lSTAFF §7!\n§7Tu veux le discord §d/discord\n",
