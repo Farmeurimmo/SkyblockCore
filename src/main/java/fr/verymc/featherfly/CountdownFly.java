f9685c8e54a194b8c2b28ef6309bdd36846f6a23
BuildTools <unconfigured@null.spigotmc.org>
2022-02-12 19:10:10 +0100

    Fix

--- src/main/java/fr/verymc/featherfly/CountdownFly.java
+++ src/main/java/fr/verymc/featherfly/CountdownFly.java
@@ -7,23 +7,32 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class CountdownFly implements Listener {
 
-    public static HashMap<String, Integer> fly = new HashMap<>();
+    public static HashMap<UUID, Integer> fly = new HashMap<>();
 
-    public static void setCooldown(String uuid, Integer time) {
+    public static CountdownFly instance;
+
+    public CountdownFly(){
+        instance = this;
+        CountDown();
+    }
+
+    public void setCooldown(UUID uuid, Integer time) {
         if (time == null)
             fly.remove(uuid);
         else
             fly.put(uuid, time);
     }
 
-    public static int getCooldown(Player player) {
-        return (fly.get(player.getName()) == null ? 0 : fly.get(player.getName()));
+    public int getCooldown(Player player) {
+        return (fly.get(player.getUniqueId()) == null ? 0 : fly.get(player.getUniqueId()));
     }
 
-    public static void EnableFlyForPlayer(Player player, String dure, String sb) {
+    public void EnableFlyForPlayer(Player player, String dure, String sb) {
         int DurationInSec = 0;
         int DurationGlobal = Integer.parseInt(sb);
         if (dure.equalsIgnoreCase("heures")) {
@@ -37,87 +46,84 @@ public class CountdownFly implements Listener {
         }
         if (getCooldown(player) >= 1) {
             int newtime = DurationInSec + getCooldown(player);
-            setCooldown(player.getName(), newtime);
+            setCooldown(player.getUniqueId(), newtime);
         } else {
-            setCooldown(player.getName(), DurationInSec);
+            setCooldown(player.getUniqueId(), DurationInSec);
             player.setAllowFlight(true);
             if (!player.getWorld().getName().equalsIgnoreCase("world")) {
                 player.setFlying(true);
             }
-            CountDown(player.getName());
         }
     }
 
-    public static void CountDown(String player) {
-        if (!fly.containsKey(player)) {
-            return;
-        }
-        if (Bukkit.getPlayer(player) != null) {
-            if (Bukkit.getPlayer(player).isOnline()) {
-                Player p = Bukkit.getPlayer(player);
-                int TimeLeft = getCooldown(p);
+    public void CountDown() {
+        for(Map.Entry<UUID, Integer> flymap : fly.entrySet()) {
+            Player player = Bukkit.getPlayer(flymap.getKey());
+            if (player != null) {
+                if (player.isOnline()) {
+                    int TimeLeft = getCooldown(player);
 
-                if (getCooldown(p) - 1 != -1 && getCooldown(p) >= 0) {
+                    if (getCooldown(player) - 1 != -1 && getCooldown(player) >= 0) {
 
-                    if (!p.getWorld().getName().equalsIgnoreCase("world")) {
-                        TimeLeft = TimeLeft - 1;
-                    }
+                        if (!player.getWorld().getName().equalsIgnoreCase("world")) {
+                            TimeLeft = TimeLeft - 1;
+                        }
 
-                    if (p.getAllowFlight() == false && !p.getWorld().getName().equalsIgnoreCase("world")) {
-                        p.setAllowFlight(true);
-                    }
-                    String aaaaaa = "" + TimeLeft;
-                    if (aaaaaa.contains("5") || aaaaaa.contains("0") || aaaaaa.contains("0") && aaaaaa.contains("5")) {
-                        Main.instance1.getDatac().set("Joueurs." + p.getName() + ".Fly.timeleft", TimeLeft);
+                        if (player.getAllowFlight() == false && !player.getWorld().getName().equalsIgnoreCase("world")) {
+                            player.setAllowFlight(true);
+                        }
+                        String aaaaaa = "" + TimeLeft;
+                        if (aaaaaa.contains("0")) {
+                            Main.instance1.getDatac().set("Joueurs." + player.getUniqueId() + ".Fly.timeleft", TimeLeft);
+                            Main.instance1.saveData();
+                        }
+
+                        int timeforconv = TimeLeft;
+                        int nHours = (timeforconv % 86400) / 3600;
+                        int nMin = ((timeforconv % 86400) % 3600) / 60;
+                        int nSec = (((timeforconv % 86400) % 3600) % 60);
+
+                        String nhoursnew;
+                        String nminnew;
+                        String nsecnew;
+                        if (nHours <= 9) {
+                            nhoursnew = "0" + nHours;
+                        } else {
+                            nhoursnew = "" + nHours;
+                        }
+                        if (nMin <= 9) {
+                            nminnew = "0" + nMin;
+                        } else {
+                            nminnew = "" + nMin;
+                        }
+                        if (nSec <= 9) {
+                            nsecnew = "0" + nSec;
+                        } else {
+                            nsecnew = "" + nSec;
+                        }
+
+                        String messagetimeleft = "§aTemps restant: " + nhoursnew + ":" + nminnew + ":" + nsecnew;
+                        CountdownFly.instance.setCooldown(player.getUniqueId(), TimeLeft);
+                        player.sendActionBar(messagetimeleft);
+
+                    } else {
+                        if (player.getGameMode() != GameMode.CREATIVE && player.getGameMode() != GameMode.SPECTATOR) {
+                            player.setAllowFlight(false);
+                            player.setFlying(false);
+                            player.chat("/is home");
+                            player.sendMessage("§6Fin du fly, Téléportation sur votre île..");
+                        }
+                        player.sendActionBar("§6Fin du fly.");
+                        fly.remove(player.getUniqueId());
+                        Main.instance1.getDatac().set("Joueurs." + player.getUniqueId() + ".Fly.timeleft", 0);
                         Main.instance1.saveData();
                     }
-
-                    int timeforconv = TimeLeft;
-                    int nHours = (timeforconv % 86400) / 3600;
-                    int nMin = ((timeforconv % 86400) % 3600) / 60;
-                    int nSec = (((timeforconv % 86400) % 3600) % 60);
-
-                    String nhoursnew;
-                    String nminnew;
-                    String nsecnew;
-                    if (nHours <= 9) {
-                        nhoursnew = "0" + nHours;
-                    } else {
-                        nhoursnew = "" + nHours;
-                    }
-                    if (nMin <= 9) {
-                        nminnew = "0" + nMin;
-                    } else {
-                        nminnew = "" + nMin;
-                    }
-                    if (nSec <= 9) {
-                        nsecnew = "0" + nSec;
-                    } else {
-                        nsecnew = "" + nSec;
-                    }
-
-                    String messagetimeleft = "§aTemps restant: " + nhoursnew + ":" + nminnew + ":" + nsecnew;
-                    CountdownFly.setCooldown(p.getName(), TimeLeft);
-                    p.sendActionBar(messagetimeleft);
-
-                } else {
-                    if (p.getGameMode() != GameMode.CREATIVE && p.getGameMode() != GameMode.SPECTATOR) {
-                        p.setAllowFlight(false);
-                        p.setFlying(false);
-                        p.chat("/is home");
-                        p.sendMessage("§6Fin du fly, Téléportation sur votre île..");
-                    }
-                    p.sendActionBar("§6Fin du fly.");
-                    fly.remove(p.getName());
-                    Main.instance1.getDatac().set("Joueurs." + p.getName() + ".Fly.timeleft", 0);
-                    Main.instance1.saveData();
-                    return;
                 }
             }
         }
-        Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Bukkit.getPluginManager().getPlugin("SkyblockCore"), new Runnable() {
+        Bukkit.getServer().getScheduler().scheduleAsyncDelayedTask(Bukkit.getPluginManager().getPlugin("SkyblockCore"), new Runnable() {
             public void run() {
-                CountDown(player);
+                CountDown();
             }
         }, 20);
     }
