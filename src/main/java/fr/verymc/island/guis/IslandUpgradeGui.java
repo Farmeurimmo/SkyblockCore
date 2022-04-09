f92b943bc9dbdb143a34f74eb609ddd73022f9f3
Farmeurimmo <robin.massonnat@gmail.com>
2022-04-09 21:05:21 +0200

    Affichage auto des % du générateur

--- src/main/java/fr/verymc/island/guis/IslandUpgradeGui.java
+++ src/main/java/fr/verymc/island/guis/IslandUpgradeGui.java
@@ -2,6 +2,8 @@ package main.java.fr.verymc.island.guis;
 
 import main.java.fr.verymc.island.Island;
 import main.java.fr.verymc.island.IslandManager;
+import main.java.fr.verymc.island.upgrade.IslandUpgradeGenerator;
+import main.java.fr.verymc.island.upgrade.IslandUpgradesType;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -9,7 +11,9 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
 
 public class IslandUpgradeGui {
 
@@ -40,11 +44,38 @@ public class IslandUpgradeGui {
         ItemStack custom2 = new ItemStack(Material.COBBLESTONE);
         ItemMeta custom2Meta = custom2.getItemMeta();
         custom2Meta.setDisplayName("§6Générateur de l'île");
-        custom2Meta.setLore(Arrays.asList("§7Générateur actuel : §6" + playerIsland.getGeneratorUpgrade().getLevel(), "", "§7Niveaux:",
-                "§70: §688% COBB, 9% IRON, 3% DIAMS", "§71: §683% COBB, 11% IRON, 2% GOLD, 5% DIAMS",
-                "§72: §678% COBB, 10% IRON, 5% GOLD, 2% LAPIS, 5% DIAMS"));
+        ArrayList<String> custom2Lore = new ArrayList<>();
+        custom2Lore.add("§7Niveau actuel : §6" + playerIsland.getGeneratorUpgrade().getLevel());
+        custom2Lore.add("");
+        custom2Lore.add("§7Niveaux: ");
+        for (int i = 0; i <= 5; i++) {
+            IslandUpgradeGenerator generator = new IslandUpgradeGenerator(i, IslandUpgradesType.GENERATOR, true);
+            generator.setLevel(i);
+            String str = "§7" + i + ": ";
+            String str2 = "";
+            for (Map.Entry<Material, Integer> entry : generator.getMaterials().entrySet()) {
+                if (str.length() <= 70) {
+                    str += "§6" + entry.getKey().name() + " §7" + entry.getValue() + "% §6";
+                } else {
+                    if (str2.isEmpty()) {
+                        str2 += "  ";
+                    }
+                    str2 += "§6" + entry.getKey().name() + " §7" + entry.getValue() + "% §6";
+                }
+            }
+            custom2Lore.add(str);
+            if (str2 != null && !str2.isEmpty()) {
+                custom2Lore.add(str2);
+            }
+        }
+        custom2Lore.replaceAll(s -> s.replace("_ORE", ""));
+        custom2Lore.replaceAll(s -> s.replace("COBBLESTONE", "COBB"));
+        custom2Lore.replaceAll(s -> s.replace("DIAMOND", "DIAMS"));
+        custom2Lore.replaceAll(s -> s.replace("EMERALD", "EMER"));
+        custom2Lore.replaceAll(s -> s.replace("ANCIENT_", ""));
+        custom2Meta.setLore(custom2Lore);
         custom2.setItemMeta(custom2Meta);
-        inv.setItem(12, custom2);
+        inv.setItem(14, custom2);
 
         ItemStack custom3 = new ItemStack(Material.PAPER);
         ItemMeta custom3Meta = custom3.getItemMeta();
@@ -53,7 +84,7 @@ public class IslandUpgradeGui {
                 "", "§7Niveau", "§70: §66 §7Membres", "§71: §68 §7Membres", "§72: §610 §7Membres", "§73: §612 §7Membres", "§74: §614 §7Membres",
                 "§75: §616 §7Membres", "", "§7Clic pour améliorer"));
         custom3.setItemMeta(custom3Meta);
-        inv.setItem(14, custom3);
+        inv.setItem(12, custom3);
 
         ItemStack custom4 = new ItemStack(Material.SPAWNER);
         ItemMeta custom4Meta = custom4.getItemMeta();
