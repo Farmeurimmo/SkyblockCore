1340e70dc5fb74ddc0d1878c634460b6eb05ea77
Farmeurimmo <farmeurimmo@gmail.com>
2022-08-04 12:04:47 +0200

    Ajout events connection/déco/kick/redirection skyblock

--- src/main/java/fr/verymc/velocity/events/ConnectionListener.java
+++ src/main/java/fr/verymc/velocity/events/ConnectionListener.java
@@ -1,10 +1,12 @@
 package main.java.fr.verymc.velocity.events;
 
 import com.velocitypowered.api.event.Subscribe;
-import com.velocitypowered.api.event.player.ServerPostConnectEvent;
+import com.velocitypowered.api.event.player.*;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
 import main.java.fr.verymc.velocity.ChannelsManager;
 import main.java.fr.verymc.velocity.Main;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 
 public class ConnectionListener {
 
@@ -23,6 +25,32 @@ public class ConnectionListener {
         ChannelsManager.instance.sendPluginMessage(e.getPlayer(), "subtp", null,
                 ChannelsManager.instance.awaitingServerSwitch.get(e.getPlayer().getUniqueId()));
         ChannelsManager.instance.awaitingServerSwitch.remove(e.getPlayer().getUniqueId());
+    }
+
+    @Subscribe
+    public void onPlayerSwitchServer(ServerPreConnectEvent e) {
+        e.getPlayer().sendMessage(Component.text("Envoi sur " + e.getOriginalServer().getServerInfo().getName() + "...", NamedTextColor.GRAY));
+    }
+
+    @Subscribe
+    public void switchServer(ServerConnectedEvent e) {
+        RegisteredServer registeredServer = Main.instance.getServeurToLogin();
+        e.getPlayer().sendMessage(Component.text("Vous avez été envoyé avec succès sur " + registeredServer.getServerInfo().getName(), NamedTextColor.GREEN));
+    }
+
+    @Subscribe
+    public void kickedFromServer(KickedFromServerEvent e) {
+        RegisteredServer registeredServer = Main.instance.getServeurToLogin();
+        e.getPlayer().createConnectionRequest(registeredServer);
+        e.getPlayer().sendMessage(Component.text("Envoi sur " + registeredServer.getServerInfo().getName() +
+                " à la suite d'un kick pour " + e.getServerKickReason(), NamedTextColor.GRAY));
+    }
+
+    @Subscribe
+    public void onPlayerChat(PlayerChooseInitialServerEvent e) {
+        if (!e.getInitialServer().isPresent()) {
+            e.setInitialServer(Main.instance.getServeurToLogin());
+        }
     }
 
 
